9b5dae5c390870151d08f35702963136d380c9cd
Vesna Groselj <vgroselj@gmail.com>
2024-09-23 17:08:20 +0200

    Initial commit of DoneTask

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { getAnalytics } from "firebase/analytics";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5Qj3cAEGje2YXC0yWWAOLvyIbOWa_CHw",
+  authDomain: "donetask-f64fe.firebaseapp.com",
+  projectId: "donetask-f64fe",
+  storageBucket: "donetask-f64fe.appspot.com",
+  messagingSenderId: "324330895675",
+  appId: "1:324330895675:web:c978d7d795ead056205f1e",
+  measurementId: "G-GT166Y7WD3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
