b5760e91e925e4196ea3ef84ec575c9bdcd33fbe
Vesna Groselj <vgroselj@gmail.com>
2024-09-24 00:11:37 +0200

    Add service worker and favicon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import './index.css';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { register } from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +13,10 @@ root.render(
   </React.StrictMode>
 );
 
+register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
